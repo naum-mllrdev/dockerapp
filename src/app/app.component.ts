bda3bef219021d0c8675bb3d58d8631d19097b0c
Noie Aum <noie.aum@mllrdev.com>
2020-10-16 08:14:18 +0800

    initial draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dockerapp';
+export class AppComponent implements OnInit {
+  globalData: any;
+  countryData: any;
+  reportDate: Date;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get(
+        'https://api.covid19api.com/summary'
+      )
+      .subscribe((response) => {
+        if (response) {
+          this.globalData = response['Global'];
+          this.countryData = response['Countries'];
+          this.reportDate = response['Date'];
+        }
+      });
+  }
 }
